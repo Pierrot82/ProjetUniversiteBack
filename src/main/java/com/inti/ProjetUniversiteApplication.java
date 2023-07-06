383f76b7a4b8821d53f25a0a3b958b8b92c01258
Pierrot82 <pierre.larroque.82@hotmail.fr>
2023-07-06 11:55:47 +0200

    Création Projet

---
+++ src/main/java/com/inti/ProjetUniversiteApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetUniversiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetUniversiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inti/ProjetUniversiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetUniversiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
