7bac2afe1dc4008e7b93f3bfac513db0240378d0
Daenrya <marjolaine.arb@gmail.com>
2023-07-06 15:30:38 +0200

    nullable

--- src/main/java/com/inti/model/Matiere.java
+++ src/main/java/com/inti/model/Matiere.java
@@ -2,6 +2,7 @@ package com.inti.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class Matiere {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idMatiere;
+	@Column(nullable = false, length = 50)
 	private String nom;
 
 }
