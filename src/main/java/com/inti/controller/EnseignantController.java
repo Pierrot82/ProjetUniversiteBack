a28a88daba0bb9acbe3ad95d3f6c72f6c1f9b87d
Thibault Sanclemente <thibault_sanclemente@icloud.com>
2023-07-07 13:22:22 +0200

    ajout crossorigin

--- src/main/java/com/inti/controller/EnseignantController.java
+++ src/main/java/com/inti/controller/EnseignantController.java
@@ -3,6 +3,7 @@ package com.inti.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.inti.model.Examen;
 import com.inti.repository.IEnseignantRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class EnseignantController {
 	
 	@Autowired
