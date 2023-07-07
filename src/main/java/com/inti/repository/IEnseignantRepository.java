6daccbae4ce83e8b272637623b20ba0bb23e0c65
Thibault Sanclemente <thibault_sanclemente@icloud.com>
2023-07-07 11:20:29 +0200

    Ajout controller et interface Enseignant

---
+++ src/main/java/com/inti/repository/IEnseignantRepository.java
@@ -0,0 +1,9 @@
+package com.inti.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.inti.model.Enseignant;
+
+public interface IEnseignantRepository extends JpaRepository<Enseignant, Integer>{
+
+}
